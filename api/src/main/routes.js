89b6d0a5ca2c6c74aa679800569fc2619aafb9d0
gabriel.missio <gabriel.missio@compasso.com.br>
2023-04-18 16:22:41 -0300

    feat: add "delete user" and "update user" routes

--- api/src/main/routes.js
+++ api/src/main/routes.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const userController = require('../presentation/controllers/user-controller')
+const userController = require('../controllers/user-controller')
 
 const router = express.Router()
 
 router.post('/users', userController.create)
 router.get('/users', userController.getAll)
 router.get('/users/:id', userController.get)
+router.put('/users/:id', userController.update)
+router.delete('/users/:id', userController.delete)
 
 module.exports = router
--- api/src/presentation/controllers/user-controller.js
+++ api/src/presentation/controllers/user-controller.js
@@ -10,7 +10,7 @@ exports.create = async (req, res) => {
     return res.status(201).json(newUser)
   } catch (error) {
     console.error(error)
-    res.status(500).json({ error: error.message || 'Server error' })
+    return res.status(500).json({ error: error.message || 'Server error' })
   }
 }
 
@@ -30,6 +30,38 @@ exports.get = async (req, res) => {
     if (!user) return res.status(404).json({ error: 'User not found' })
 
     return res.status(200).json(user)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ error: error.message || 'Server error' })
+  }
+}
+
+exports.update = async (req, res) => {
+  try {
+    const user = await userRepository.getById(req.params.id)
+    if (!user) return res.status(404).json({ error: 'User not found' })
+
+    const changeEmail = req.body.email && req.body.email !== user.email
+    if (changeEmail) {
+      const existingUser = await userRepository.getByEmail(req.body.email)
+      if (existingUser) return res.status(409).json({ error: 'Email already exists' })
+    }
+
+    await userRepository.updateById(req.params.id, req.body)
+    const updatedUser = await userRepository.getById(req.params.id)
+
+    return res.status(200).json(updatedUser)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ error: error.message || 'Server error' })
+  }
+}
+
+exports.delete = async (req, res) => {
+  try {
+    await userRepository.deleteById(req.params.id)
+
+    return res.status(204).json({})
   } catch (error) {
     console.error(error)
     return res.status(500).json({ error: error.message || 'Server error' })
